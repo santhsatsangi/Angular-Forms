080d006d85ffc89ce7c8cd5dd3c53dc7d614c160
DESKTOP-K6KJ9PM\Admin <prasanthsatsangi@gmail.com>
2024-03-04 09:34:38 -0800

    feature: UI part done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: 'Home', component: LoginComponent},
+  { path: '**', redirectTo: 'Home' }
+];
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
