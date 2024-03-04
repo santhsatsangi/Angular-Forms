5e7a29f5d5748f876eddce019946175f8b520340
DESKTOP-K6KJ9PM\Admin <prasanthsatsangi@gmail.com>
2024-03-04 10:18:29 -0800

    feature: Login Model logic completed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,24 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ FormsModule],
+  imports: [ FormsModule,ReactiveFormsModule,JsonPipe],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+   constructor(private Fb :FormBuilder){}
+   loginForm = this.Fb.group({
+    userName : [''],
+    password : ['']
+  });
+
+  onSubmit(){
+    console.log("Login Data", this.loginForm.value)
+  }
 
 }
